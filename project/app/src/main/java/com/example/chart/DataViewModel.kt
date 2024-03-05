a7b7455fe404961554c786b92d9f9dae8368ec69
Freya0119 <103048480+Freya0119@users.noreply.github.com>
2024-03-05 16:28:15 +0800

    Add more line

--- project/app/src/main/java/com/example/chart/DataViewModel.kt
+++ project/app/src/main/java/com/example/chart/DataViewModel.kt
@@ -2,13 +2,11 @@ package com.example.chart
 
 import android.util.Log
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.chart.data.Stock
 import com.example.chart.data.StockData
 import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import retrofit2.Call
@@ -31,24 +29,6 @@ class DataViewModel() : ViewModel() {
         dataSets.value = listOf(LineDataSet(emptyList(), "NONE"))
     }
 
-    fun updateDataSets(entryList: List<Entry>) {
-        val sets = mutableListOf<ILineDataSet>()
-        sets.add(LineDataSet(entryList, "LabelA"))
-        dataSets.value = sets
-//        Log.d(
-//            DATA_MSG, "entry: ${entry.value?.first()}, " +
-//                    "sets: ${sets.first()?.entryCount}, " +
-//                    "datasets: ${dataSets.value?.first()?.entryCount}"
-//        )
-    }
-
-    fun setLifecycle(lifecycleOwner: LifecycleOwner) {
-        dataList.observe(lifecycleOwner) {
-            Log.d(DATA_MSG, "Observe")
-            getEntryList(STOCK_ID)  // 好多list
-        }
-    }
-
     private fun getStock() {
         val api = ServiceApi.getter.getStockResult(STOCK_ID)
         api.enqueue(object : Callback<StockData> {
@@ -65,29 +45,52 @@ class DataViewModel() : ViewModel() {
         })
     }
 
-    private fun getEntryList(stockId: Int) {
+    private fun getEntry(stockId: Int) {
         val riverMap = dataList.value?.find { it.stockCode == stockId }?.riverMapInfo
         if (riverMap != null) {
-            if (riverMap.isNotEmpty()) {
-                Log.d(DATA_MSG, "Began entry")
-//                val date = riverMap.map { it?.yearMonth?.div(100) ?: 0 }  // data -> x軸
-                val price = riverMap.map { it?.closingPriceMonth ?: 0f }    // price -> y軸  // 倒過來，軸依賴
-                val lowestPer = riverMap.map { it?.perStockPriceStandard?.get(0) ?: 0f }  // lowest基準
-                val lowerPer = riverMap.map { it?.perStockPriceStandard?.get(1) ?: 0f }  // lowest基準
-                val mediumLowerPer = riverMap.map { it?.perStockPriceStandard?.get(2) ?: 0f }  // lowest基準
-                val mediumHigherPer = riverMap.map { it?.perStockPriceStandard?.get(3) ?: 0f }  // lowest基準
-                val higherPer = riverMap.map { it?.perStockPriceStandard?.get(4) ?: 0f }  // lowest基準
-                val highestPer = riverMap.map { it?.perStockPriceStandard?.get(5) ?: 0f }  // lowest基準
+            val entryLists = mutableListOf<List<Entry>>()
+            //                entry.value = entryList
+            //                Log.d(DATA_MSG, "finish entry: ${entryList?.size}")
+            //                Log.d(DATA_MSG, "Began entry")
+            //                val date = riverMap.map { it?.yearMonth?.div(100) ?: 0 }  // data -> x軸
 
-                val indexArr = MutableList(price.size) {
-                    it.toFloat()
-                }
-                val entryList = indexArr.zip(lowerPer) { x, y -> Entry(x.toFloat(), y) }
-//                entry.value = entryList
-//                Log.d(DATA_MSG, "finish entry: ${entryList?.size}")
+            val price = riverMap.map { it?.closingPriceMonth ?: 0f }    // price -> y軸  // 倒過來，軸依賴
 
-                updateDataSets(entryList)
+            val indexArr = MutableList(price.size) {
+                it.toFloat()
             }
+            val priceEntryList = indexArr.zip(price) { x, y -> Entry(x.toFloat(), y) }
+            entryLists.add(priceEntryList)
+
+            val perSize = 6 - 1 // 河流圖
+            for (i in 0..perSize) {
+                val perList = riverMap.map { it?.perStockPriceStandard?.get(i) ?: 0f }  // lowest基準
+                val entryList = indexArr.zip(perList) { x, y -> Entry(x, y) }
+                entryLists.add(entryList)
+            }
+
+            Log.d(DATA_MSG, "Entry list size: ${entryLists.size}")
+            updateDataSets(entryLists)
+        }
+    }
+
+    private fun updateDataSets(entryList: List<List<Entry>>) {
+        val sets = mutableListOf<ILineDataSet>()
+        var count = 0
+        for (list in entryList) {
+            count++
+            sets.add(LineDataSet(list, "Label $count}"))
+        }
+        setLineData(sets)
+    }
+
+    private fun setLineData(sets: List<ILineDataSet>) {
+        dataSets.value = sets
+    }
+
+    fun setLifecycle(lifecycleOwner: LifecycleOwner) {
+        dataList.observe(lifecycleOwner) {
+            getEntry(STOCK_ID)  // 好多list
         }
     }
 }
