ba732e176a6a8bd9566342343548eab59bf6c07d
Freya0119 <103048480+Freya0119@users.noreply.github.com>
2024-03-09 15:43:42 +0800

    1.3 add ChartColor.kt

---
+++ project/app/src/main/java/com/example/chart/data/ChartColor.kt
@@ -0,0 +1,13 @@
+package com.example.chart.data
+
+import android.graphics.Color
+
+val colors = listOf(
+    Color.RED,
+    Color.parseColor("#FF8000"),
+    Color.parseColor("#FFFF00"),
+    Color.parseColor("#80FF00"),
+    Color.parseColor("#00FFFF"),
+    Color.parseColor("#0000FF"),
+    Color.parseColor("#FF00FF")
+)
